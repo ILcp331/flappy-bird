f3e71d08b459e2eee4c6d5180c4ff3407bde29a6
ILcp331 <lennonliu1100@gmail.com>
2026-09-18 14:18:03 +0800

    Make bird flap onlt when space is pressed, not held. Adjust bird movement values accordingly.

--- src/sprites/bird.py
+++ src/sprites/bird.py
@@ -7,8 +7,8 @@ class Bird(pg.sprite.Sprite):
     WIDTH = 32
     HEIGHT = 32
 
-    FLAP_STRENGTH = -10
-    GRAVITY = 0.8
+    FLAP_STRENGTH = -12
+    GRAVITY = 0.75
     TERMINAL_VEL = 24
 
     def __init__(
@@ -27,10 +27,24 @@ class Bird(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = self.pos
 
-    def move(self):
+        self.space_key_pressed = False
+        self.space_key_last_state = False
+
+    def get_input(self):
         keys = pg.key.get_pressed()
 
         if keys[pg.K_SPACE]:
+            if self.space_key_last_state:
+                self.space_key_pressed = False
+            else:
+                self.space_key_pressed = True
+
+        self.space_key_last_state = keys[pg.K_SPACE]
+
+    def move(self):
+        keys = pg.key.get_pressed()
+
+        if self.space_key_pressed:
             self.vel.y = self.FLAP_STRENGTH
 
         self.vel.y += self.GRAVITY
@@ -39,6 +53,7 @@ class Bird(pg.sprite.Sprite):
         self.pos += self.vel
 
     def update(self):
+        self.get_input()
         self.move()
 
         self.rect.x, self.rect.y = self.pos
