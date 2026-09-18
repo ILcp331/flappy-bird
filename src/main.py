18c41c15f99a6776f6a6f2c88f1380b18aafcb39
ILcp331 <lennonliu1100@gmail.com>
2026-09-18 14:07:57 +0800

    Implement player sprite and movement logic.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 
 from .constants import WIDTH, HEIGHT, FPS, COLOR_SKY
+from .sprites.bird import Bird
 
 pg.init()
 
@@ -9,10 +10,16 @@ class Game:
     def __init__(self):
         self.window = pg.display.set_mode((WIDTH, HEIGHT))
         self.clock = pg.time.Clock()
+        self.sprites = pg.sprite.Group()
 
         pg.display.set_caption('Flappy Bird')
 
+        self.bird = Bird((WIDTH/4, 0), self.sprites)
+
         self.running = True
+
+    def draw_sprites(self):
+        self.sprites.draw(self.window)
 
     def update(self):
         self.window.fill(COLOR_SKY)
@@ -20,6 +27,9 @@ class Game:
         for e in pg.event.get():
             if e.type == pg.QUIT:
                 self.running = False
+
+        self.bird.update()
+        self.draw_sprites()
 
         pg.display.update()
         self.clock.tick(FPS)
---
+++ src/sprites/bird.py
@@ -0,0 +1,44 @@
+import pygame as pg
+
+pg.init()
+
+
+class Bird(pg.sprite.Sprite):
+    WIDTH = 32
+    HEIGHT = 32
+
+    FLAP_STRENGTH = -10
+    GRAVITY = 0.8
+    TERMINAL_VEL = 24
+
+    def __init__(
+        self,
+        pos: tuple[float, float],
+        *groups
+    ):
+        super().__init__(*groups)
+
+        self.pos = pg.Vector2(pos)
+        self.vel = pg.Vector2(0, 0)
+
+        self.image = pg.Surface((self.WIDTH, self.HEIGHT))
+        self.image.fill((0, 0, 0))
+
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = self.pos
+
+    def move(self):
+        keys = pg.key.get_pressed()
+
+        if keys[pg.K_SPACE]:
+            self.vel.y = self.FLAP_STRENGTH
+
+        self.vel.y += self.GRAVITY
+        self.vel.y = max(min(self.vel.y, self.TERMINAL_VEL), -self.TERMINAL_VEL)
+
+        self.pos += self.vel
+
+    def update(self):
+        self.move()
+
+        self.rect.x, self.rect.y = self.pos
