b3ad9998e414ab78967c660a33cbe9ae67b169e1
ILcp331 <lennonliu1100@gmail.com>
2026-09-22 08:56:10 +0800

    Add flapping cooldown to bird. (bugfix)

--- src/sprites/bird.py
+++ src/sprites/bird.py
@@ -10,6 +10,7 @@ class Bird(pg.sprite.Sprite):
     COLOR = (255, 240, 189)
 
     FLAP_STRENGTH = -10
+    FLAP_COOLDOWN = 2
     GRAVITY = 0.75
     TERMINAL_VEL = 24
 
@@ -31,6 +32,7 @@ class Bird(pg.sprite.Sprite):
 
         self.space_key_pressed = False
         self.space_key_last_state = False
+        self.frames_since_last_flap = 0
 
     def get_input(self):
         keys = pg.key.get_pressed()
@@ -44,8 +46,11 @@ class Bird(pg.sprite.Sprite):
         self.space_key_last_state = keys[pg.K_SPACE]
 
     def move(self):
-        if self.space_key_pressed:
+        if self.space_key_pressed and self.frames_since_last_flap > self.FLAP_COOLDOWN:
             self.vel.y = self.FLAP_STRENGTH
+            self.frames_since_last_flap = 0
+        else:
+            self.frames_since_last_flap += 1
 
         self.vel.y += self.GRAVITY
         self.vel.y = max(min(self.vel.y, self.TERMINAL_VEL), -self.TERMINAL_VEL)
